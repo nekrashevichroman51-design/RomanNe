6ba14403b472088328d8a4356023a7505e22b483
nekrashevichroman51-design <nekrashevichroman51@gmail.com>
2026-09-25 19:02:08 +0300

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    document.getElementById('mobile-nav').classList.toggle('open');
+});
+
+document.querySelectorAll('#mobile-nav .close-mobile-nav').forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.getElementById('mobile-nav')?.classList.remove('open');
+    });
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(link => {
     link.addEventListener('click', e => {
+        const hash = link.hash;
+        if (!hash || window.location.pathname !== link.pathname) return;
         e.preventDefault();
-        const targetId = document.getElementById(link.getAttribute('href').substring(1));
-        if (!targetId) return;
-        
+        const targetEl = document.querySelector(hash);
+        if (!targetEl) return;
         // Учитываем высоту хедера
         const headerHeight = document.querySelector('.site-header').getBoundingClientRect().height;
         window.scrollTo({
-            top: targetId.offsetTop - headerHeight - 20,
+            top: targetEl.offsetTop - headerHeight,
             behavior: 'smooth'
         });
+        // Закрываем мобильное меню при клике по ссылке
+        document.getElementById('mobile-nav')?.classList.remove('open');
     });
 });
